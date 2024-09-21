fa1cf2ae15966b3e35f562a2d7069a53f2f8b9aa
Malleswaribai <mudavathmalleswari123@gmail.com>
2024-09-21 14:25:33 +0100

    Added concept.js and .gitigonre files

---
+++ NodeJs/concept.js
@@ -0,0 +1,16 @@
+/* 
+ Node: Node is a javascript run time environment . It will run the stand alone javascript code outside the browser. These day node is getting popular for server side coding. 
+ To create a node project we need a package.json file which will maintain the versions of node packages. this file will be responcible to download required project to that pirticular project.
+ Package: It is a javascript library where there exists pre writtened code which will make the developer life easier. It is like stl in c++ ;
+
+ to set up package.json in the folder , we need to run a command npm init. 
+ npm stands for node package manager.
+ there are lot of packages on the internet. the most popular one that we use is express.
+
+ to install a package we need to run command npm install package_name. 
+
+ When we run the above command a folder is created node_modules, this folder contains all the package code. this will be downloaded from the internet when we run npm install. 
+ 
+ We need to create a file with name .gitignore , inside this file we need to mention node_modules so that these files won't be tracked by git. 
+ 
+*/
